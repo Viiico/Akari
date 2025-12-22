09405b0d16215f28d0356dd08d89ecd80cfd8e9a
Viiico <wiktorkwv@gmail.com>
2025-12-23 00:27:49 +0100

    Inizjalizacja projektu

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-4xl">Hello World!</h1>
+  <NuxtLink to="/about">About</NuxtLink>
+</template>
+
+<script setup>
+useHead({
+  title: 'Akari Game',
+});
+</script>
+
+<style>
+
+</style>
