d921fd9f97891252f19d13db516066d0c3699de1
Viiico <wiktorkwv@gmail.com>
2026-01-11 19:23:46 +0100

    Setup pod uzyskiwanie danych z bazy danych

--- app/pages/Leaderboards.vue
+++ app/pages/Leaderboards.vue
@@ -1,11 +1,17 @@
 <template>
   <NavigationPanel />
-    <h1>Leaderboards</h1>
+  <h1>Leaderboards</h1>
+  <p v-if="pending">Loading...</p>
+  <p v-if="error"> An error has occured: {{ error.data.message }}</p>
+  <ul v-else class="list-disc list-inside p-4">
+    <li v-for="fruit in data?.fruits" :key="fruit.id">
+      {{ fruit.name }}
+    </li>
+  </ul>
 </template>
 
-<script>
+<script setup>
+const { data, pending, error } = useFetch('/api/hello', {server: false});
 </script>
 
-<style>
-
-</style>
+<style></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,13 @@
 <template>
     <NavigationPanel />
     <h1 class="text-4xl">Hello World!</h1>
+    {{ counter }}
+    <button @click="counter++">Click</button>
 </template>
 
 <script setup>
+const counter = useState('counter', () => 0);
+
 useHead({
     title: 'Akari Game',
 });
