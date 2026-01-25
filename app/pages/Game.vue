8732a2439c50351b904e2aabc866748a52938cf3
Twoje Imię <magdalena.waszczuk.stud@pw.edu.pl>
2026-01-25 15:16:47 +0100

    I jeszcze mala poprawka

--- app/pages/Game.vue
+++ app/pages/Game.vue
@@ -20,7 +20,7 @@
             <div class="mt-8 flex flex-wrap gap-3 justify-center">
                 <button @click="printDiv('board')"
                     class="button px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md transition-colors disabled:bg-gray-500">
-                    Print game board
+                    Print the game board
                 </button>
             </div>
         </div>
