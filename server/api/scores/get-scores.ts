b1c4e6db532904c51fe4a1f970a54103aedfa818
Viiico <wiktorkwv@gmail.com>
2026-01-25 02:38:03 +0100

    Dodanie oblugi parametru uzyskiwanych danych

--- server/api/scores/get-scores.ts
+++ server/api/scores/get-scores.ts
@@ -1,11 +1,39 @@
 import { scoreTable, usersTable } from "../../db/schema";
 import { useDrizzle } from "../../utils/drizzle";
-import { desc, eq } from "drizzle-orm";
+import { desc, eq, gte } from "drizzle-orm";
+import { LeaderBoardPeriods as LeaderboardPeriodsType } from "../../../app/lib/Types/Generic";
 
 export default defineEventHandler(async (event) => {
   const db = useDrizzle();
-  
-  const scores = await db
+
+  const query = getQuery(event);
+  const period =
+    (query.type as LeaderboardPeriodsType) || LeaderboardPeriodsType.ALL_TIME;
+
+    let dateFilter = new Date(0);
+
+  switch (period) {
+    case LeaderboardPeriodsType.DAILY: {
+      const yesterday = new Date();
+      yesterday.setDate(yesterday.getDate() - 1);
+      dateFilter = yesterday;
+      break;
+    }
+
+    case LeaderboardPeriodsType.WEEKLY: {
+      const lastWeek = new Date();
+      lastWeek.setDate(lastWeek.getDate() - 7);
+      dateFilter = lastWeek;
+      break;
+    }
+
+    case LeaderboardPeriodsType.ALL_TIME: {
+      // No date filter
+      break;
+    }
+  }
+
+  let baseQuery = db
     .select({
       id: scoreTable.id,
       userId: scoreTable.userId,
@@ -16,10 +44,13 @@ export default defineEventHandler(async (event) => {
     })
     .from(scoreTable)
     .leftJoin(usersTable, eq(scoreTable.userId, usersTable.id))
-    .orderBy(desc(scoreTable.score))
-    .all();
+    .where(gte(scoreTable.createdAt, dateFilter));
+
+  const scores = await baseQuery.orderBy(desc(scoreTable.score)).all();
 
   return {
     scores,
+    period,
+    totalCount: scores.length,
   };
 });
