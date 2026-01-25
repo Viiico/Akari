8bfd676a28acb6525a34b811d7215dd0dcfc7c1a
wwasak <01178701@pw.edu.pl>
2026-01-25 01:54:05 +0100

    Pozycja print

--- app/pages/Game.vue
+++ app/pages/Game.vue
@@ -17,7 +17,7 @@
             </div>
                 <div class="mt-8 flex flex-wrap gap-3 justify">
                     <button @click="printDiv('board')"
-                        class="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md transition-colors disabled:bg-gray-500">
+                        class="button px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md transition-colors disabled:bg-gray-500">
                         Print
                     </button>
             </div>
@@ -150,4 +150,10 @@ function printDiv(divName: string) {
     max-width: 1200px;
     margin: 0 auto;
 }
+.button{
+    position: relative;
+    left:80%;
+}
+
+
 </style>
